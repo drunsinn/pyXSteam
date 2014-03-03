050f8307a022a2b2b82852f936900dfb54e349b7
Max Pirkl <pirkl.max@googlemail.com>
2014-03-03 11:10:18 +0100

    removed eclipse comments

--- pyXSteam/__init__.py
+++ pyXSteam/__init__.py
@@ -1,6 +1,2 @@
 # -*- coding: utf-8 -*-
-'''
-Created on 03.02.2014
 
-@author: max
-'''
--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-'''
-Created on 03.02.2014
-
-@author: max
-'''
 from setuptools import setup
 
 
