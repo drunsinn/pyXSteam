1cdf183e81dda7fc93cdb1e63095d16bf303bec0
Max Pirkl <pirkl.max@googlemail.com>
2014-03-03 11:28:01 +0100

    test zipsafe option for setup

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
                     ],
     platforms = ('Any',),
     packages = ['pyXSteam', 'test'],
+    zip_safe = True,
     scripts = ['bin'],
     url = '',
     license = 'LICENSE.txt',
