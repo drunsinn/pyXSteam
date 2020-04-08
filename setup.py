ba63df67a7a3b34a7816e372e124b6d57c7595e2
drunsinn <dr.unsinn@googlemail.com>
2020-04-08 11:02:10 +0200

    rebase to master

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyXSteam",
-    version="0.4.3", # version number for documentation does not auto-update
+    version="0.4.4", # version number for documentation does not auto-update
     author="drunsinn",
     author_email="dr.unsinn@googlemail.com",
     keywords="steam water ice XSteam",
