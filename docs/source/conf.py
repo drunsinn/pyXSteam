a95cb67cb5828e0087fc82390ccd487a358187c5
drunsinn <dr.unsinn@googlemail.com>
2020-06-23 16:30:12 +0200

    update news and bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'drunsinn, Magnus Holmgren'
 # The short X.Y version
 version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.4.3'
+release = '0.4.4'
 
 
 # -- General configuration ---------------------------------------------------
