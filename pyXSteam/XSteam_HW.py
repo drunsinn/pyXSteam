6d534136ff848d585fa86a236c4217de90359b8c
Max <max@chp1.net>
2023-03-31 20:32:21 +0200

    add surface tention accesible and make unit converter private

--- pyXSteam/XSteam_HW.py
+++ pyXSteam/XSteam_HW.py
@@ -10,6 +10,7 @@ from .Constants import (
 )
 from .UnitConverter import UnitConverter
 from .IAPWS_R4 import myHW_rhoT_R4, tcHW_rhoT_R4
+from .IAPWS_R5 import surface_tension_T
 
 
 class XSteam_HW:
@@ -29,29 +30,29 @@ class XSteam_HW:
         Constructor method
         """
         self.logger = logging.getLogger("pyXSteam:Heavy Water")
-        self.unit_converter = UnitConverter(unitSystem)
+        self._unit_converter = UnitConverter(unitSystem)
         self.logger.info(
             "initialised pyXSteam for Heavy Water with Unit System %s",
-            self.unit_converter,
+            self._unit_converter,
         )
 
     def criticalTemperatur(self):
         """
         :return: specific temperature with conversion to the selected unit system
         """
-        return self.unit_converter.fromSIunit_T(CRITICAL_TEMPERATURE_D20_1992)
+        return self._unit_converter.fromSIunit_T(CRITICAL_TEMPERATURE_D20_1992)
 
     def criticalPressure(self):
         """
         :return: specific pressure with conversion to the selected unit system
         """
-        return self.unit_converter.fromSIunit_p(CRITICAL_PRESSURE_D20_1992)
+        return self._unit_converter.fromSIunit_p(CRITICAL_PRESSURE_D20_1992)
 
     def criticalDensity(self):
         """
         :return: specific density with conversion to the selected unit system
         """
-        return self.unit_converter.fromSIunit_p(CRITICAL_DENSITY_D20_1992)
+        return self._unit_converter.fromSIunit_p(CRITICAL_DENSITY_D20_1992)
 
     def my_rhoT(self, rho: float, T: float) -> float:
         """Viscosity as a function of density and temperature for heavy water
@@ -74,14 +75,14 @@ class XSteam_HW:
                 "negative values for density rho not allowed %f", rho)
             raise ValueError("rho out of range")
 
-        rho = self.unit_converter.toSIunit_p(rho)
-        T = self.unit_converter.toSIunit_T(T)
+        rho = self._unit_converter.toSIunit_p(rho)
+        T = self._unit_converter.toSIunit_T(T)
 
         if T < 277.0 or T > 775.0:
             self.logger.error("temperature out of range")
             return float("NaN")
 
-        return self.unit_converter.fromSIunit_T(myHW_rhoT_R4(rho, T))
+        return self._unit_converter.fromSIunit_T(myHW_rhoT_R4(rho, T))
 
     def tc_rhoT(self, rho: float, T: float) -> float:
         """Thermal conductivity as a function of density and temperature for heavy water
@@ -104,11 +105,22 @@ class XSteam_HW:
                 "negative values for density rho not allowed %f", rho)
             raise ValueError("rho out of range")
 
-        rho = self.unit_converter.toSIunit_p(rho)
-        T = self.unit_converter.toSIunit_T(T)
+        rho = self._unit_converter.toSIunit_p(rho)
+        T = self._unit_converter.toSIunit_T(T)
 
         if T < 277.0 or T > 825.0:
             self.logger.error("temperature out of range")
             return float("NaN")
 
-        return self.unit_converter.fromSIunit_tc(tcHW_rhoT_R4(rho, T))
+        return self._unit_converter.fromSIunit_tc(tcHW_rhoT_R4(rho, T))
+
+    def st_t(self, t: float) -> float:
+        """
+        Surface tension for two phase water/steam as a function of temperature
+
+        :param t: temperature
+
+        :return: surface tension
+        """
+        T = self._unit_converter.toSIunit_T(t)
+        return self._unit_converter.fromSIunit_st(surface_tension_T(T))
